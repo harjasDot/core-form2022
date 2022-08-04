c0b9287ea0bb3fd064dfa9c8d69f4bb5fb0a193e
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-08-04 22:57:43 +0530

    update

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class QUESTION(models.Model):
+    name=models.CharField(blank=False,max_length=50)
+    q1=models.CharField(blank=False,max_length=500)
+    q2=models.CharField(blank=False,max_length=500)
+    q3=models.CharField(blank=False,max_length=500)
+    q4=models.CharField(blank=False,max_length=500)
+    q5=models.CharField(blank=False,max_length=500)
+    q6=models.CharField(blank=False,max_length=500)
+    q7=models.CharField(blank=False,max_length=500)
+    q8=models.CharField(blank=False,max_length=500)
+    q9=models.CharField(blank=False,max_length=500)
+    content=models.IntegerField(blank=False,default=9)
+    tech=models.IntegerField(blank=False,default=9)
+    design=models.IntegerField(blank=False,default=9)
+    cultural=models.IntegerField(blank=False,default=9)
+    creativity=models.IntegerField(blank=False,default=9)
+    hospitality=models.IntegerField(blank=False,default=9)
+    logistics=models.IntegerField(blank=False,default=9)
+
+
+   
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import requests
+from app.models import QUESTION
 # Create your views here.
 sheetly_header={
     "Authorization":"Bearer csrvdvbteadvbrav"
@@ -8,16 +8,44 @@ sheetly_endpoint="https://api.sheety.co/f94482e928c78729c2052c3d0c34b12c/sheet/w
 
 def core(request):
     if request.method=='POST':
-        sheetly_params = {
-        "workout": {
-            "name":request.POST['name'],
-            "entry1":request.POST['entry1'],
-            "entry2":request.POST['entry2'],
-        }
-        }
+        name=request.POST['name']
+        q1=request.POST['q1']
+        q2=request.POST['q2']
+        q3=request.POST['q3']
+        q4=request.POST['q4']
+        q5=request.POST['q5']
+        q6=request.POST['q6']
+        q7=request.POST['q7']
+        q8=request.POST['q8']
+        q9=request.POST['q9']
+        content=int(request.POST['content'])
+        tech=int(request.POST['tech'])
+        design=int(request.POST['design'])
+        cultural=int(request.POST['cultural'])
+        creativity=int(request.POST['creativity'])
+        hospitality=int(request.POST['hospitality'])
+        logistics=int(request.POST['logistics'])
+        data=QUESTION.objects.create(
+        name=name,
+        q1=q1,
+        q2=q2,
+        q3=q3,
+        q4=q4,
+        q5=q5,
+        q6=q6,
+        q7=q7,
+        q8=q8,
+        q9=q9,
+        content=int(content),
+        tech=int(tech),
+        design=int(design),
+        cultural=int(cultural),
+        creativity=int(creativity),
+        hospitality=int(hospitality),
+        logistics=int(logistics),
+        )
+        data.save()
 
-        response = requests.post(url=sheetly_endpoint, json=sheetly_params, headers=sheetly_header)
-        # print(response.text)
         return render(request,'thankyou.html')
     return render(request,'core.html')
 
