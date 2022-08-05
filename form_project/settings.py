16765849f71f19b29c99f64e101bdfdde08aa5e0
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-08-05 21:55:41 +0530

    api

--- form_project/settings.py
+++ form_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ekj6uq@^r@x#o_toeuh!*#h!$$ust&75y$t4n@6w^a153kmala'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["core-form.herokuapp.com","*","http://127.0.0.1:8000/","https://virsa.azurewebsites.net"]
 
